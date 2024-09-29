06bb6af9dfe396da8f78de0582306ed757e15e66
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2024-09-29 09:24:08 +0530

    Create find all symmetric pairs in the array of pairs.cpp

---
+++ find all symmetric pairs in the array of pairs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n = 5;
+	int arr[5][2] = {{1, 2}, {2, 1}, {3, 4}, {4, 5}, {5, 4}};
+  unordered_map<int,int>mp;
+  for(int i=0;i<n;i++){
+  int first=arr[i][0];
+    int second=arr[i][1];
+    if(mp.find(second)!=mp.end() && mp[second]==first){
+      cout<<"("<<first<<second<<")"<<endl;
+    }
+    else{
+      mp[first]=second;
+    }
+  }
+	return 0;
+}
