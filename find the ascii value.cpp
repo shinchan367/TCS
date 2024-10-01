34341f18670bf029640d1bc10bd188af038ddb64
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2024-10-01 19:39:20 +0530

    Create find the ascii value.cpp

---
+++ find the ascii value.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class ASCII {
+
+  public:
+
+    void value() {
+
+      char c = 'A';
+      cout << "The ASCII value of " << c << " is " << int(c);
+    }
+};
+int main() {
+
+  ASCII p1;
+  p1.value();
+
+  return 0;
+}
