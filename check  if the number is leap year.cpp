9f1a9607f880cfeb30869e0da942615f79e1222b
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2024-10-03 11:06:12 +0530

    Create check  if the number is leap year.cpp

---
+++ check  if the number is leap year.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int year = 2000;
+  if((year%4==0 && year%100!=0) || year%400==0){
+{
+  cout<<"true";
+}
+    else{
+      cout<<"false;
+        }
+  }
+return 0;
+}
